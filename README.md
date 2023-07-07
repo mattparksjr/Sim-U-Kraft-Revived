f224143b8b9358832a215a2377e1a3308162394a
Matthew Parks <mattparksjr@gmail.com>
2023-07-07 19:06:04 -0400

    fix typo, add all light blocks except rainbow

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Sim-U-Kraft Revived
 -------------------------------------------
 This project is an attempt to revive the once popular Sim-U-Kraft Mod. It was originally made
-for mining 1.6.4 by Scott Hather, and then later revived and updated to 1.12.2 by the Resimulators.
+for minecraft 1.6.4 by Scott Hather, and then later revived and updated to 1.12.2 by the Resimulators.
 However, both of these versions of the mod are no longer maintained or updated. The plan is to bring
-Sim-U-Kraft to a modern mincraft version, and to keep it that way. Part of the reason the mod was
+Sim-U-Kraft to a modern minecraft version, and to keep it that way. Part of the reason the mod was
 never updated in the first place, was the massive API changes that forge went through over the years.
 
 Contributing:
--- src/main/java/codes/matthewp/sukr/init/BlockInit.java
+++ src/main/java/codes/matthewp/sukr/init/BlockInit.java
@@ -5,6 +5,10 @@ import codes.matthewp.sukr.block.BlockConstructor;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,6 +17,35 @@ public class BlockInit {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SimUKraft.MODID);
     public static final RegistryObject<Block> BLOCK_CONSTRUCTOR = BLOCKS.register("block_constructor", BlockConstructor::new);
-    public static final RegistryObject<Item> BLOCK_CONSTRUCTOR_ITEM = ItemInit.ITEMS.register("block_constructor", () -> new BlockItem(BLOCK_CONSTRUCTOR.get(), new Item.Properties()));
+    public static final RegistryObject<Block> BLOCK_LIGHT = BLOCKS.register("block_light",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
+    public static final RegistryObject<Block> BLOCK_LIGHT_RED = BLOCKS.register("block_light_red",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
+    public static final RegistryObject<Block> BLOCK_LIGHT_GREEN = BLOCKS.register("block_light_green",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
+    public static final RegistryObject<Block> BLOCK_LIGHT_BLUE = BLOCKS.register("block_light_blue",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
+    public static final RegistryObject<Block> BLOCK_LIGHT_ORANGE = BLOCKS.register("block_light_orange",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
+    public static final RegistryObject<Block> BLOCK_LIGHT_PURPLE = BLOCKS.register("block_light_purple",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
+    public static final RegistryObject<Block> BLOCK_LIGHT_YELLOW = BLOCKS.register("block_light_yellow",() -> new Block(BlockBehaviour.Properties.of()
+            .mapColor(MapColor.SNOW)
+            .lightLevel((e) -> 10)));
 
+
+    public static final RegistryObject<Item> BLOCK_CONSTRUCTOR_ITEM = ItemInit.ITEMS.register("block_constructor", () -> new BlockItem(BLOCK_CONSTRUCTOR.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM = ItemInit.ITEMS.register("block_light", () -> new BlockItem(BLOCK_LIGHT.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM_RED = ItemInit.ITEMS.register("block_light_red", () -> new BlockItem(BLOCK_LIGHT_RED.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM_GREEN = ItemInit.ITEMS.register("block_light_green", () -> new BlockItem(BLOCK_LIGHT_GREEN.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM_BLUE = ItemInit.ITEMS.register("block_light_blue", () -> new BlockItem(BLOCK_LIGHT_BLUE.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM_ORANGE = ItemInit.ITEMS.register("block_light_orange", () -> new BlockItem(BLOCK_LIGHT_ORANGE.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM_PURPLE = ItemInit.ITEMS.register("block_light_purple", () -> new BlockItem(BLOCK_LIGHT_PURPLE.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BLOCK_LIGHT_ITEM_YELLOW = ItemInit.ITEMS.register("block_light_yellow", () -> new BlockItem(BLOCK_LIGHT_YELLOW.get(), new Item.Properties()));
 }
